ad55c2a2a75f9f812e53c1bb779274bef26ee354
fedorkolomensky <115565719+fedorkolomensky@users.noreply.github.com>
2023-01-22 23:32:05 +0300

    Update task_2.py

--- Лабораторная работа 2/task_2.py
+++ Лабораторная работа 2/task_2.py
@@ -13,23 +13,41 @@ BOOKS_DATABASE = [
 
 class Book:
     def __init__(self, id_: int, name: str, pages: int):
+        """
+        Создание и подготовка к работе объекта "Книга"
+        :param id_: Идентификатор (порядковый номер) книги
+        :param name: Название книги
+        :param pages: Количество страниц в книге
+        """
         self.id_ = id_
         self.name = name
         self.pages = pages
 
 class Library:
     def __init__(self, books=None):
+        """
+        Создание и подготовка к работе объекта "Библиотека"
+        :param books: Список книг
+        """
         if books is None:
             books = []
         self.books = books
 
     def get_next_book_id (self, id_) -> int:
+        """"
+        Метод возвращает идентификатор новой книги для добавления в библиотеку, если в библиотеке есть книги.
+        Метод возвращает 1, если в библиотеке нет книг.
+        """
         if len(self.books) == 0:
             return 1
         else:
             return self.books[-1].id_+1
 
     def get_index_by_book_id (self, id_) -> int:
+        """
+        Метод возвращает индекс книги по идентификатору, если книга с данным id существует.
+        Метод возвращает строку "Книги с запрашиваемым id не существует", если такой книги нет.
+        """
         for index, book in enumerate(self.books):
             if book.id_ == id_:
                 return index
